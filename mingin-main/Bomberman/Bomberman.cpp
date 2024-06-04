0aa3c5da7aca47e677357b492f085a0db8d9340a
Alexander Terentyev <olek.terentyev@gmail.com>
2024-06-04 20:27:46 +0200

    Added box2d

--- mingin-main/Bomberman/Bomberman.cpp
+++ mingin-main/Bomberman/Bomberman.cpp
@@ -17,6 +17,13 @@
 #include <SDL_events.h>
 #include <SDL_mixer.h>
 
+////BOX2d Includes
+#include "box2d/base.h"
+#include "box2d/box2d.h"
+#include "box2d/collision.h"
+#include "box2d/math_functions.h"
+
+
 ////////////////////////////////////////////
 #include <stdexcept>
 
