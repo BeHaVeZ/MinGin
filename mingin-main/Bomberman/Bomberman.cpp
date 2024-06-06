e23ea67b1ba04b4bec6de20d1e2d4f4d6de42f6c
Alexander Terentyev <olek.terentyev@gmail.com>
2024-06-06 14:09:22 +0200

    Filtered out the engine files

--- mingin-main/Bomberman/Bomberman.cpp
+++ mingin-main/Bomberman/Bomberman.cpp
@@ -18,10 +18,7 @@
 #include <SDL_mixer.h>
 
 ////BOX2d Includes
-#include "box2d/base.h"
 #include "box2d/box2d.h"
-#include "box2d/collision.h"
-#include "box2d/math_functions.h"
 
 
 ////////////////////////////////////////////
