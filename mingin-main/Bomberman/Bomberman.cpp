bc23615c6fd0f9eb03511ac2658507f586d9d48a
Alexander Terentyev <olek.terentyev@gmail.com>
2024-06-06 18:16:09 +0200

    Cleared project

--- mingin-main/Bomberman/Bomberman.cpp
+++ mingin-main/Bomberman/Bomberman.cpp
@@ -17,8 +17,7 @@
 #include <SDL_events.h>
 #include <SDL_mixer.h>
 
-////BOX2d Includes
-#include "box2d/box2d.h"
+
 
 
 ////////////////////////////////////////////
@@ -92,28 +91,19 @@ void load()
     go->GetTransform().SetScale(3.f, 3.f, 1.f);
     scene.Add(go);
 
-    go = CREATE_GAMEOBJECT();
-    go->AddComponent<TextureComponent>("logo.tga");
-    go->SetPosition(216, 180);
-    scene.Add(go);
 
-    auto font = ResourceManager::GetInstance().LoadFont("Lingua.otf", 36);
-    auto to = CREATE_GAMEOBJECT();
-    to->AddComponent<TextObject>("Press 'U' to play a sound!", font);
-    to->GetComponent<TextObject>()->SetPosition(20, 20);
-    to->SetPosition(60, 20);
-    scene.Add(to);
+    auto font = ResourceManager::GetInstance().LoadFont("bb.otf", 12);
 
     auto fpsObject = CREATE_GAMEOBJECT();
     fpsObject->AddComponent<FPSCounter>(font);
     scene.Add(fpsObject);
 
-
     auto player1 = CREATE_GAMEOBJECT();
-    player1->AddComponent<TextureComponent>("bomb.png");
-    player1->AddComponent<HealthComponent>(3);
+    player1->AddComponent<TextureComponent>("bomberman.png");
+    player1->GetTransform().SetScale(2.5f, 2.5f, 1.f);
+    player1->AddComponent<HealthComponent>(4);
     player1->AddComponent<ScoreComponent>();
-    player1->SetPosition(100.f, 100.f);
+    player1->SetPosition(40.f, 40.f);
 
     auto textObjectLives = CREATE_GAMEOBJECT();
     textObjectLives->AddComponent<TextObject>("Lives player 1: 3", font);
@@ -124,7 +114,7 @@ void load()
     auto weakHealthComponent = player1->GetComponent<HealthComponent>();
 
     weakHealthComponent->GetOnDeathEvent()->AddListener([textObjectLivesComponent](int lives) {
-        textObjectLivesComponent->SetText("Lives player 1: " + std::to_string(lives));
+        textObjectLivesComponent->SetText("Lives: " + std::to_string(lives));
         });
 
 
@@ -140,21 +130,7 @@ void load()
         textObjectScoreComponent->SetText("Score: " + std::to_string(score));
         });
 
-
-    scene.Add(player1);
-
     const float moveSpeed{ 20.f };
-    auto moveCommand = std::make_shared<MoveCommand>(player1, glm::lowp_vec3{ 0.f,-1.f,0.f }, moveSpeed);
-    Input::GetInstance().AddCommand(std::make_pair(SDL_SCANCODE_W, SDL_KEYMAPCHANGED), moveCommand);
-
-    moveCommand = std::make_shared<MoveCommand>(player1, glm::vec3{ -1.f, 0.f, 0.f }, moveSpeed);
-    Input::GetInstance().AddCommand(std::make_pair(SDL_SCANCODE_A, SDL_KEYMAPCHANGED), moveCommand);
-
-    moveCommand = std::make_shared<MoveCommand>(player1, glm::vec3{ 0.f, 1.f, 0.f }, moveSpeed);
-    Input::GetInstance().AddCommand(std::make_pair(SDL_SCANCODE_S, SDL_KEYMAPCHANGED), moveCommand);
-
-    moveCommand = std::make_shared<MoveCommand>(player1, glm::vec3{ 1.f, 0.f, 0.f }, moveSpeed);
-    Input::GetInstance().AddCommand(std::make_pair(SDL_SCANCODE_D, SDL_KEYMAPCHANGED), moveCommand);
 
     auto killCommand = std::make_shared<KillCommand>(player1->GetComponent<HealthComponent>());
     Input::GetInstance().AddCommand(std::make_pair(SDL_SCANCODE_E, SDL_KEYDOWN), killCommand);
@@ -162,57 +138,29 @@ void load()
     Input::GetInstance().AddCommand(std::make_pair(SDL_SCANCODE_R, SDL_KEYDOWN), addScoreCommand);
 
 
-    auto player2 = CREATE_GAMEOBJECT();
-    player2->AddComponent<TextureComponent>("bomberman.png");
-    player2->GetTransform().SetScale(2.5f, 2.5f, 1.f);
-    player2->AddComponent<HealthComponent>(3);
-    player2->AddComponent<ScoreComponent>();
-    player2->SetPosition(200.f, 200.f);
-
-    auto textObjectLivesPlayer2 = CREATE_GAMEOBJECT();
-    textObjectLivesPlayer2->AddComponent<TextObject>("Lives player 2: 3", font);
-    textObjectLivesPlayer2->SetPosition(350, 300.f);
-    scene.Add(textObjectLivesPlayer2);
-
-    auto textObjectScorePlayer2 = CREATE_GAMEOBJECT();
-    textObjectScorePlayer2->AddComponent<TextObject>("Score: 0", font);
-    textObjectScorePlayer2->SetPosition(350.f, 350.f);
-    scene.Add(textObjectScorePlayer2);
-
-    auto textObjectLivesPlayer2Component = textObjectLivesPlayer2->GetComponent<TextObject>();
-    auto weakHealthComponentPlayer2 = player2->GetComponent<HealthComponent>();
-    weakHealthComponentPlayer2->GetOnDeathEvent()->AddListener([textObjectLivesPlayer2Component](int lives) {
-        textObjectLivesPlayer2Component->SetText("Lives player 2: " + std::to_string(lives));
-        });
-
-    auto textObjectScorePlayer2Component = textObjectScorePlayer2->GetComponent<TextObject>();
-    auto weakScoreComponentPlayer2 = player2->GetComponent<ScoreComponent>();
-    weakScoreComponentPlayer2->GetOnScoreChangedEvent()->AddListener([textObjectScorePlayer2Component](int score) {
-        textObjectScorePlayer2Component->SetText("Score: " + std::to_string(score));
-        });
-
-
     Input::GetInstance().AddController(std::make_shared<GamePad>(0));
 
-    moveCommand = std::make_shared<MoveCommand>(player2, glm::vec3{ 0.f, -1.f, 0.f }, moveSpeed * 4.f);
+    auto moveCommand = std::make_shared<MoveCommand>(player1, glm::vec3{ 0.f, -1.f, 0.f }, moveSpeed * 4.f);
     Input::GetInstance().AddCommand(std::make_tuple(0, GamePad::ControllerButton::DPadUp, KeyState::Pressed), moveCommand);
 
-    moveCommand = std::make_shared<MoveCommand>(player2, glm::vec3{ 0.f, 1.f, 0.f }, moveSpeed * 4.f);
+    moveCommand = std::make_shared<MoveCommand>(player1, glm::vec3{ 0.f, 1.f, 0.f }, moveSpeed * 4.f);
     Input::GetInstance().AddCommand(std::make_tuple(0, GamePad::ControllerButton::DPadDown, KeyState::Pressed), moveCommand);
 
-    moveCommand = std::make_shared<MoveCommand>(player2, glm::vec3{ -1.f, 0.f, 0.f }, moveSpeed * 4.f);
+    moveCommand = std::make_shared<MoveCommand>(player1, glm::vec3{ -1.f, 0.f, 0.f }, moveSpeed * 4.f);
     Input::GetInstance().AddCommand(std::make_tuple(0, GamePad::ControllerButton::DPadLeft, KeyState::Pressed), moveCommand);
 
-    moveCommand = std::make_shared<MoveCommand>(player2, glm::vec3{ 1.f, 0.f, 0.f }, moveSpeed * 4.f);
+    moveCommand = std::make_shared<MoveCommand>(player1, glm::vec3{ 1.f, 0.f, 0.f }, moveSpeed * 4.f);
     Input::GetInstance().AddCommand(std::make_tuple(0, GamePad::ControllerButton::DPadRight, KeyState::Pressed), moveCommand);
 
-    killCommand = std::make_shared<KillCommand>(player2->GetComponent<HealthComponent>());
+    killCommand = std::make_shared<KillCommand>(player1->GetComponent<HealthComponent>());
     Input::GetInstance().AddCommand(std::make_tuple(0, GamePad::ControllerButton::ButtonSouth, KeyState::Down), killCommand);
 
-    addScoreCommand = std::make_shared<AddScoreCommand>(player2->GetComponent<ScoreComponent>());
+    addScoreCommand = std::make_shared<AddScoreCommand>(player1->GetComponent<ScoreComponent>());
     Input::GetInstance().AddCommand(std::make_tuple(0, GamePad::ControllerButton::ButtonWest, KeyState::Down), addScoreCommand);
 
-    scene.Add(player2);
+    scene.Add(player1);
+
+
 
     ///DECORATOR
 #if _DEBUG
